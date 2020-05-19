326ef3c7ea8f3a96e5476bc0737cb6187a06d288
Douglas Curtis <docurtis@redhat.com>
2020-05-19 16:48:09 -0400

    updating masu-server, removing worker constraint

--- run_internal.sh
+++ run_internal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sleep 15
 # python koku/manage.py migrate_schemas
-DJANGO_READ_DOT_ENV_FILE=True python koku/manage.py runserver 0.0.0.0:8000
+DJANGO_READ_DOT_ENV_FILE=True gunicorn 'koku.wsgi:application' --bind=0.0.0.0:8080 --config gunicorn.py

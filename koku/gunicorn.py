e649a8bd93b77fb0d8ff304e611f14f6e2e313a3
Douglas Curtis <docurtis@redhat.com>
2020-05-20 12:46:55 -0400

    perf thread/cpu changes

--- koku/gunicorn.py
+++ koku/gunicorn.py
@@ -11,7 +11,9 @@ SOURCES = ENVIRONMENT.bool("SOURCES", default=False)
 bind = "unix:/var/run/koku/gunicorn.sock"
 cpu_resources = int(os.environ.get("POD_CPU_LIMIT", multiprocessing.cpu_count()))
 workers = 1 if SOURCES else cpu_resources * 2 + 1
-# threads = 10
+threads = 4
+# worker_class="gevent"
+# worker_connections=1
 timeout = int(os.environ.get("TIMEOUT", "90"))
 loglevel = os.environ.get("LOG_LEVEL", "INFO")
 graceful_timeout = int(os.environ.get("GRACEFUL_TIMEOUT", "180"))

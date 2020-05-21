136c4dd3bbebfde5fca17fac02fd54b7ce47025b
Douglas Curtis <docurtis@redhat.com>
2020-05-21 14:11:33 -0400

    profiling

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 sleep 5
 python manage.py migrate_schemas
+#mkdir -p /testing/profile/
+#python manage.py runprofileserver 0.0.0.0:8080 --use-cprofile --prof-path=/koku/koku
 #DJANGO_READ_DOT_ENV_FILE=True python koku/manage.py runserver 0.0.0.0:8000
 DJANGO_READ_DOT_ENV_FILE=True gunicorn 'koku.wsgi:application' --bind=0.0.0.0:8080 --config gunicorn.py
